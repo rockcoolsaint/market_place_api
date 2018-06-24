45929cea257ebc487847401de05ae5d4cc08aed5
Innocent <rockcoolsaint@gmail.com>
2018-06-24 01:04:13 +0000

    Adds shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}
-  
-  before { @user = FactoryBot.build(:user) }
-  
-  subject { @user }
-
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  
-  it { should be_valid }
+    # pending "add some examples to (or delete) #{__FILE__}
+      
+    before { @user = FactoryBot.build(:user) }
+      
+    subject { @user }
+    
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:password) }
+    it { should validate_confirmation_of(:password_confirmation) }
+    it { should allow_value('example@domain.com').for(:email) }
+      
+    it { should be_valid }
+    
+    describe "when email is not present" do
+        #before { @user = FactoryBot.build(:user) }
+        before { @user.email = " " }
+      
+        it { should_not be_valid }
+    end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,9 +26,20 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
